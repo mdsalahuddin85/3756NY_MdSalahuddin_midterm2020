c24104ca70294b5c8c476cbf33b7f43fd08edbed
mdsalahuddin85 <mdsrobin1985@gmail.com>
2020-01-13 17:13:01 -0500

    Math Problem solved

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -13,6 +13,16 @@ public class MakePyramid {
                          * * * * * *
 
         */
+        for (int i = 0; i<5; i++){
+            for (int j = 0; j<5-i; j++ ){
+                System.out.print(" ");
+            }
+
+            for(int k = 0; k<=i; k++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
 
 
         }
