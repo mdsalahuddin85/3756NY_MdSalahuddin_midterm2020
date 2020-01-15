ef9a5a83d0b5d6fc510c2efe1ef231ecd898cab8
mdsalahuddin85 <mdsrobin1985@gmail.com>
2020-01-15 16:30:56 -0500

    math commit

--- src/math/problems/Pattern.java
+++ src/math/problems/Pattern.java
@@ -5,30 +5,42 @@ import java.util.List;
 
 public class Pattern {
 
-	public static void main(String[] args) {
-		/* Read this numbers, find the pattern then implement the logic from this pattern.which will give you this output.
-		 * 100,99,98,97,96,95,94,93,92,91,90,88,86,84,82,80,78,76,74,72,70,67,64,61,58,55,52,49,46,43,40,36,32............
-		 *
-		 *
-		 */
-		int count = 1, a = 0;
-		int num = 100;
-		List<String> storePattern = new ArrayList<>();
-		int[] array = new int[40];
+    public static void main(String[] args) {
+        /* Read this numbers, find the pattern then implement the logic from this pattern.which will give you this output.
+         * 100,99,98,97,96,95,94,93,92,91,90,88,86,84,82,80,78,76,74,72,70,67,64,61,58,55,52,49,46,43,40,36,32............
+         *
+         *
+         */
+        int count = 0;
+        for (int i = 100; i >= 0; i--) {
+            count++;
+            if (count <= 10) {
+                System.out.println(i + " ");
+            } else {
+                for (i = 90; i >= 0; i -= 2) {
+                    if (count > 10 && count <= 20) {
+                        System.out.println(i + " ");
+                        count++;
+                    } else {
+                        for (i = 70; i >= 0; i -= 3) {
+                            if (count > 20 && count <= 30) {
+                                System.out.println(i + " ");
+                                count++;
+                            } else {
+                                for (i = 40; i >= 0; i -= 4) {
+                                    if (count > 30 && count <= 40) {
+                                        System.out.println(i + " ");
+                                        count++;
+                                    }
+                                }
+                            }
 
-		while (num > 0) {
-			for (int i = 0; i < 10; i++) {
-				System.out.print(" " + num);
-				array[a] = num;
-				num -= count;
-				a++;
-
-			}
-			count++;
-		}
-		System.out.println(" ");
-	}
-
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 
 }
