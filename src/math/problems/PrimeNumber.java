3ba72366d9539b753127fb1a1009c8780f4a4eec
mdsalahuddin85 <mdsrobin1985@gmail.com>
2020-01-15 21:11:31 -0500

    updated

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -16,7 +16,7 @@ public class PrimeNumber {
 		int num = 0;
 		String primeNumbers = "";
 
-		for (i = 1; i <= 1000000; i++) {
+		for (i = 1; i <= 100; i++) {
 			int counter = 0;
 			for (num = i; num >= 1; num--) {
 				if (i % num == 0) {
